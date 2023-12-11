c190797b4c984f4799286a1b2b332ff5ccebdba9
onkar <onkar@gmail.com>
2023-12-11 09:51:56 +0530

    Please complete given task....

---
+++ src/com/java/main/Apis_Testing.java
@@ -0,0 +1,17 @@
+package com.java.main;
+
+public class Apis_Testing {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=5; i++)
+		{
+			System.out.println("Suceess...........");
+		}
+		
+		System.out.println("For git & git trial");
+	
+		//Task for for Mr Sagar Ullagadde
+		// Write the code of addition
+	}
+
+}
