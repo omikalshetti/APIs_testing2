daaedf80b6ec8e3f6d4b4a896b9546049ec392cc
New <New@DESKTOP-UGMF6HI>
2023-12-11 18:44:31 +0530

    add all

---
+++ src/com/java/main/NEwclass.java
@@ -0,0 +1,14 @@
+package com.java.main;
+
+public class NEwclass {
+
+	
+	
+	public static void m1()
+	{
+		System.out.println("hello omkar i have completed task");
+		System.out.println("hello omkar i have completed task");
+		System.out.println("Please check this and approve me for marge");
+	}
+	
+}
